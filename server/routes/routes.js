1ffc457eb796ba1e852a61557c02d564e9a41918
kabalisa innocent fiston <ikabalisa20@gmail.com>
2018-11-21 10:23:12 -0500

    [feature #161862590] user can fetch one parcel

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,4 +6,7 @@ const router = express.Router();
 /* GET all parcel delivery orders */
 router.get('/parcels', parcelController.getAll);
 
+/* GET a specific Parcel delivery order*/
+router.get('/parcels/:id', parcelController.getOne);
+
 export default router;
--- server/test/routesTest.js
+++ server/test/routesTest.js
@@ -38,4 +38,39 @@ it('should FETCH all parcels', (done) => {
   });
 });
 
+//test the GET one parcel endpoint
+it('should FETCH one specific parcel', (done) => {
+  chai.request(app)
+  .get('/api/v1/parcels/1')
+  .end((err, res) => {
+   res.should.have.status(200);
+   res.body.should.be.a('object');
+   res.body.should.have.property('id').eql('1');
+   res.body.should.have.property('weight').eql('4 kg');
+   res.body.should.have.property('price').eql('4000');
+   res.body.should.have.property('pickup').eql('nyamirambo');
+   res.body.should.have.property('pickup_StNo').eql('KN 245 St');
+   res.body.should.have.property('destination').eql('remera');
+   res.body.should.have.property('destination_StNo').eql('KN 478 St');
+   res.body.should.have.property('userId').eql('980768');
+   res.body.should.have.property('receiver').eql('mucyo');
+   res.body.should.have.property('receiver_phone').eql('0788465633');
+   res.body.should.have.property('status').eql('pending');
+   res.body.should.have.property('presentLocation').eql('kinamba');
+   done();
+  });
+});
+
+//test when not to fetch a single parcel
+it('should not FETCH one specific parcel', (done) => {
+  chai.request(app)
+  .get('/api/v1/parcels/8999')
+  .end((err, res) => {
+     res.should.have.status(404);
+     res.body.should.be.a('object');
+     res.body.should.have.property('message').eql('parcel do not exist');
+     done();
+  });
+});
+
 });
