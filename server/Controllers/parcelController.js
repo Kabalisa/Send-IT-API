a1362b1fd1f8989f04bbfcd37f22011776ba8fdb
kabalisa innocent fiston <ikabalisa20@gmail.com>
2018-11-28 09:10:37 -0500

    [feature #161862612] user can cancel a specific parcel delivery order

--- server/Controllers/parcelController.js
+++ server/Controllers/parcelController.js
@@ -1,72 +1,55 @@
-import parcelData from '../Data/parcelData';
+import query from '../Data/parcelData';
 
 const parcel = {
 
-create(req, res){
-  if(!req.body.pickup_StNo || !req.body.pickup || !req.body.destination_StNo || !req.body.destination || !req.body.weight || !req.body.userId || !req.body.receiver || !req.body.receiver_phone){
-  	return res.status(400).send({message: 'complete all fields to proceed'});
+async cancelOrder(req, res){
+
+let sql = `
+UPDATE parcels
+SET status = $1
+WHERE id = $2
+returning *
+`;
+
+let data = [
+"CANCELED",
+req.params.id
+];
+
+let sql2 = `
+SELECT * FROM parcels
+WHERE id = $1
+`;
+
+let data2 = [req.params.id];
+
+if(!req.body.userId){
+  return res.status(404).send({message:'complete all fields to proceed'});  
+}
+
+
+let { rows } = await query(sql2, data2);
+
+if(rows[0].userid !== Number.parseInt(req.body.userId)){
+     return res.status(400).send({message:'the specified user did not create this parcel'});
   }
-  const parcel = parcelData.create(req.body);
-  return res.status(201).send(parcel);
-},
 
-getAll(req, res){
-const allParcels = parcelData.getAll();
-return res.status(200).send(allParcels);
-},
+try{
 
-getOne(req, res){
-const oneParcel = parcelData.getOne(req.params.id);
-if(!oneParcel){
-	return res.status(404).send({message: 'parcel do not exist'});
-}
-else{
-  return res.status(200).send(oneParcel);
+  let { rows } = await query(sql, data);
+  
+  if(!rows[0]){
+    return res.status(400).send({message:'the parcel does not exist'});
+  }
+  else{
+    return res.status(200).send(rows[0]);
+  }
+
 }
 
-},
-
-delete(req, res){
- // const parcel = parcelData.getOne(req.params.id);
-const note  = parcelData.delete(req.params.id);
- if (!note){
- 	return res.status(404).send({message: 'parcel do not exist'});
- }
-   return res.status(201).send({message : 'parcel DELETED'});
-},
-
-getAllUserParcels(req, res){
-	const parcels = parcelData.getAllUserParcels(req.params.id);
-	if(parcels.length === 0){
-    return res.status(404).send({message: 'user has no parcels'});
-	}
-	else{
-		return res.status(200).send(parcels);
-	}
-},
-
-cancelOrder(req, res){
-const parcel = parcelData.cancelOrder(req.params.id);
-if(!parcel){
-  return res.status(404).send({message:'parcel do not exist'});	
+catch(error){
+  return res.status(400).send(error.message);
 }
-else{
-	return res.status(200).send(parcel);
-}
-},
-
-updateOrder(req, res){
-if(req.body.weight =="" && req.body.pickup == "" && req.body.pickup_StNo == "" && req.body.destination == "" && req.body.destination_StNo == "" && req.body.receiver == "" && req.body.receiver_phone == ""){
-  return res.status(404).send({message:'nothing updated'});
-}
-
-const updated = parcelData.updateOrder(req.params.id, req.body);
-
-if(!updated){
-  return res.status(404).send({message:'parcel do not exist'});
-}
-
-return res.status(200).send(updated);
 
 }
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,12 +3,6 @@ import parcelController from '../Controllers/parcelController';
 
 const router = express.Router();
 
-/* GET all parcel delivery orders */
-router.get('/parcels', parcelController.getAll);
-
-/* GET a specific Parcel delivery order*/
-router.get('/parcels/:id', parcelController.getOne);
-
 /*cancel a specific parcel delivery order*/
 router.put('/parcels/:id/cancel', parcelController.cancelOrder);
 
