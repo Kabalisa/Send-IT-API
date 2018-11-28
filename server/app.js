4c6d7dfd0b91b1f9374e968541e461a935ead616
kabalisa innocent fiston <ikabalisa20@gmail.com>
2018-11-28 01:18:13 -0500

    [bug #162102762] Localhost 3000

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ import routes from './routes/routes';
 
 const app = express();
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 3000; 
 
 // const test = database;
 // console.log(database);
