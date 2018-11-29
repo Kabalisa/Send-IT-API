a9b79cbb374bb26125086d1f71d41d1a71e52321
kabalisa innocent fiston <ikabalisa20@gmail.com>
2018-11-29 09:26:06 -0500

    [bug #162305856] implement JWT on signup

--- server/helpers/bhelp.js
+++ server/helpers/bhelp.js
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import query from '../Data/parcelData';
 
 let bhelp = {
 
@@ -9,7 +11,33 @@ let bhelp = {
   
   validatePassword(inputpassword, hashedpassword){
      return bcrypt.compareSync(inputpassword, hashedpassword);
+  },
+
+  makeToken(id){
+  	const token = jwt.sign({ id }, 'fadees', { expiresIn : '2h' });
+  	return token;
+  },
+
+  async checkToken(req, res, next){
+  	const token = req.headers['tokenToProceed'];
+  	if(!token){
+  		return res.status(403).send({message:'No Token provided'});
+  	}
+  	try{
+        const { id } = await jwt.verify(token, 'fadees');
+        let sql = 'SELECT * FROM users WHERE userid = $1';
+        let { rows } = await query(sql, [id]);
+        if(!rows[0]){
+        	return res.status(400).send({message:'user does not exist'});
+        }
+        req.body.userId = id;
+        next();
+  	}
+    catch(error){
+       return res.status(400).send(error.message);
+    }
   }
+
 };
 
 export default bhelp;
